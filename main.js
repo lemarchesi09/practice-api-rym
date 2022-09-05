681e2f9886e4534fff4912930ff905f510d9be4f
lemarchesi09 <lemarchesi9@gmail.com>
2022-09-05 09:29:08 -0300

    :sparkles: Feat: new feature SearchBar working

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const container = document.querySelector('.contenedor')
 const body = document.querySelector('body')
 const select = document.querySelector('#selector')
 const btnDetails = document.querySelector('#buttonDetails')
+const form = document.querySelector('form')
+const searchBar = document.getElementById('search-bar')
+console.log(searchBar.value);
 
 //      ------GENERAL--------
 // Get Random page for landing page
@@ -52,25 +55,25 @@ const seleccionar = ()=>{
 
             fetch(`${URL_API}/?gender=male`)
             .then((response) => response.json())
-            .then((data) => mostrarGenero (data.results))
+            .then((data) => showData (data.results))
             console.log('Male');
             break;
         case 2:
             fetch(`${URL_API}/?gender=female`)
             .then((response) => response.json())
-            .then((data) => mostrarGenero (data.results))
+            .then((data) => showData (data.results))
             console.log('Female');
             break;
         case 3:
             fetch(`${URL_API}/?gender=unknown`)
             .then((response) => response.json())
-            .then((data) => mostrarGenero (data.results))
+            .then((data) => showData (data.results))
             console.log('Unknown');
             break;
         case 4:
             fetch(`${URL_API}/?gender=genderless`)
             .then((response) => response.json())
-            .then((data) => mostrarGenero (data.results))
+            .then((data) => showData (data.results))
             console.log('Genderless');
             break;
     
@@ -78,31 +81,8 @@ const seleccionar = ()=>{
             break;
     }
 }
-//          ITERAR ELEMENTO
-const mostrarGenero = (array) =>{
-    let card = ``
-    for (const element of array) {
-        console.log(element);
-        const {name, image, gender, species, status, id} = element
-        
-        card += `
-        <div class="col-md-6 col-xl-4">
-            <div class="tarjeta">
-                <h2>${name}</h2>
-                <img class="img-fluid" src="${image}">
-                <p><b>Gender:</b> ${gender}</p>
-                <p><b>Species:</b> ${species}</p>
-                <p><b>Status:</b> ${status}</p>
-                <p><b>Location:</b> ${element.location.name}</p>
-                <button onclick="callDetails(${id})" type="button" class="btn btn-primary boton">Details</button>
-            </div>
-        </div>
-        `
-    }
-    
 
-    container.innerHTML = card
-}
+
 
 //          MOSTRAR DETALLES
 const callDetails = (id) =>{
@@ -137,5 +117,22 @@ const showCard = (info)=>{
     `
     container.innerHTML = card
 }
+
+//          FILTRAR PERSONAJES
+// https://rickandmortyapi.com/api/character/?name=rick
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    const filterCharacter = () =>{
+        const search = searchBar.value;
+        fetch(`${URL_API}/?name=${search}`)
+        .then((response) => response.json())
+        .then((data) => showData(data.results))
+        form.reset()
+    }
+    
+    filterCharacter()
+    
+})
 
 
